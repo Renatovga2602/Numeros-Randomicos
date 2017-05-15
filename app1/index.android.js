ffc68da0e2a00b5d3c80ff4582c4a946083d20f0
Renato <renatovalerio25@gmail.com>
2017-05-15 00:50:38 -0300

    function Num Randômicogit add .

--- app1/index.android.js
+++ app1/index.android.js
@@ -1,12 +1,28 @@
 var React = require ('react');
 var Text = require('react-native').Text;
-var AppRegistry =  require('react-native').AppRegistry;
+var View = require('react-native').View;
+var Button = require('react-native').Button;
+var AppRegistry = require('react-native').AppRegistry;
 
+const numAleatorio = () => {
+  var num_aleatorio = Math.random();
+  
+  num_aleatorio = Math.floor(num_aleatorio *10);
+  alert(num_aleatorio);
+
+}
 const App = () => {
 
   return (
-    <Text>Primeiro APP</Text>
+    <View>
+      <Text>Gerador de Números Randômicos</Text>
+      <Button
+        title = "Gerar número randômico"
+        onPress={numAleatorio}
+      />
+    </View>
   );
 };
 
-AppRegistry.registerComponent('app1',() =>  App);
+AppRegistry.registerComponent('app1', () => App);
+
