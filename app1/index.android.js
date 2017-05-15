db191f97c76ce851a8c705922854e01d8104b520
Renato <renatovalerio25@gmail.com>
2017-05-15 00:21:30 -0300

    Modification index.android.js

---
+++ app1/index.android.js
@@ -0,0 +1,12 @@
+var React = require ('react');
+var Text = require('react-native').Text;
+var AppRegistry =  require('react-native').AppRegistry;
+
+const App = () => {
+
+  return (
+    <Text>Primeiro APP</Text>
+  );
+};
+
+AppRegistry.registerComponent('app1',() =>  App);
